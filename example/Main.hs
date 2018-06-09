0c711403535b84dadc65b725c8eb1ddeb991d5cd
myuon <ioi.joi.koi.loi@gmail.com>
2018-06-09 11:07:10 -0700

    feat(lefrect): simple lefrect framework

--- example/Main.hs
+++ example/Main.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE OverloadedStrings, OverloadedLabels, TypeApplications #-}
 module Main where
 
-import qualified SDL as SDL
 import Control.Monad
 import Data.Extensible
 import Linear.V2
@@ -9,39 +8,23 @@ import Linear.V4
 import Graphics.UI.Lefrect
 
 main :: IO ()
-main = do
-  SDL.initializeAll
-  window <- SDL.createWindow "Application" SDL.defaultWindow
-  renderer <- SDL.createRenderer window (-1) SDL.defaultRenderer
+main = runUI $ do
   counter <- create @_ @"counter"
   counterView <- view counter
 
-  loop renderer counterView
+  ui (GridLayout 50 50) $ translate (V2 100 100) $ graphics $
+    [ colored (V4 30 30 30 255) $ graphics $
+      [ rectangle (V2 0 0) (V2 1 1)
+      , rectangle (V2 1 1) (V2 1 1)
+      ]
+    , colored (V4 255 255 255 255) $ graphics $
+      [ rectangle (V2 1 0) (V2 1 1)
+      , rectangle (V2 0 1) (V2 1 1)
+      ]
+    , colored (V4 200 100 100 255) $ rectangleWith (#fill @= False <: nil) (V2 4 4) (V2 1 1)
+    , colored (V4 200 100 100 255) $ rectangleWith (#rounded @= Just 10 <: nil) (V2 5 5) (V2 1 1)
+    , translate (V2 200 0) counterView
+    ]
 
-  where
-    loop renderer counterView = do
-      events <- SDL.pollEvents
-      keyQuit <- return $ flip any events $ \ev -> case SDL.eventPayload ev of
-        SDL.KeyboardEvent (SDL.KeyboardEventData _ _ _ (SDL.Keysym SDL.ScancodeQ _ _)) -> True
-        _ -> False
-
-      SDL.rendererDrawColor renderer SDL.$= SDL.V4 0 0 255 255
-      SDL.clear renderer
-
-      render renderer (GridLayout 50 50) $ translate (V2 100 100) $ graphics $
-        [ colored (V4 30 30 30 255) $ graphics $
-          [ rectangle (V2 0 0) (V2 1 1)
-          , rectangle (V2 1 1) (V2 1 1)
-          ]
-        , colored (V4 255 255 255 255) $ graphics $
-          [ rectangle (V2 1 0) (V2 1 1)
-          , rectangle (V2 0 1) (V2 1 1)
-          ]
-        , colored (V4 200 100 100 255) $ rectangleWith (#fill @= False <: nil) (V2 4 4) (V2 1 1)
-        , colored (V4 200 100 100 255) $ rectangleWith (#rounded @= Just 10 <: nil) (V2 5 5) (V2 1 1)
-        , translate (V2 200 0) counterView
-        ]
-
-      SDL.present renderer
-      unless keyQuit $ loop renderer counterView
+  mainloop
 
--- src/Graphics/UI/Lefrect.hs
+++ src/Graphics/UI/Lefrect.hs
@@ -1,8 +1,69 @@
+{-# LANGUAGE OverloadedStrings, GeneralizedNewtypeDeriving #-}
 module Graphics.UI.Lefrect
   ( module Graphics.UI.Lefrect.Graphical
   , module Graphics.UI.Lefrect.Component
+
+  , UI
+  , runUI
+  , ui
+  , clear
+  , mainloop
   ) where
 
+import qualified SDL as SDL
+import Control.Monad.State
+import Control.Lens
+import Data.Word
+import Linear.V4
 import Graphics.UI.Lefrect.Graphical
 import Graphics.UI.Lefrect.Component
+
+newtype UI a = UI { unpackUI :: StateT UIState IO a } deriving (Functor, Applicative, Monad, MonadIO)
+
+data UIState
+  = UIState
+  { _window :: SDL.Window
+  , _renderer :: SDL.Renderer
+  , _uiRegistry :: [(Layout, Graphical)]
+  }
+
+makeLenses ''UIState
+
+runUI :: UI () -> IO ()
+runUI m = do
+  SDL.initializeAll
+  window <- SDL.createWindow "window" SDL.defaultWindow
+  renderer <- SDL.createRenderer window (-1) SDL.defaultRenderer
+
+  evalStateT (unpackUI m) $ UIState
+    { _window = window
+    , _renderer = renderer
+    , _uiRegistry = []
+    }
+
+ui :: Layout -> Graphical -> UI ()
+ui layout graphical = UI $ do
+  uiRegistry %= ((layout, graphical) :)
+
+clear :: V4 Word8 -> UI ()
+clear c = UI $ do
+  r <- use renderer
+  SDL.rendererDrawColor r SDL.$= c
+  SDL.clear r
+
+mainloop :: UI ()
+mainloop = do
+  events <- SDL.pollEvents
+  keyQuit <- return $ flip any events $ \ev -> case SDL.eventPayload ev of
+    SDL.KeyboardEvent (SDL.KeyboardEventData _ _ _ (SDL.Keysym SDL.ScancodeQ _ _)) -> True
+    _ -> False
+
+  clear (V4 30 100 200 255)
+
+  r <- UI $ use renderer
+  mapM_ (uncurry (render r)) =<< UI (use uiRegistry)
+
+  SDL.present =<< UI (use renderer)
+
+  unless keyQuit mainloop
 
